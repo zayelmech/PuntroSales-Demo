86f9fc7294aeba01a2ca3c5a56f79e73ce5d4a97
Abdiel Carreño <i.equipo001@gmail.com>
2023-02-22 23:58:57 -0500

    Checkout ticket

--- demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/add/views/CheckoutTicketComposable.kt
+++ demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/add/views/CheckoutTicketComposable.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +21,19 @@ import com.imecatro.demosales.ui.theme.Typography
 
 @Composable
 fun CheckoutTicketComposable(
-    list: List<ProductOnCartUiModel>
+    list: List<ProductOnCartUiModel>,
+    client: String,
+    onChangeClientClick: () -> Unit,
+    note: String,
+    onAddNoteClick: () -> Unit,
+    shipping: String,
+    onChangeShippingCostClick: () -> Unit,
+    tax: String,
+    onChangeTaxClick: () -> Unit,
+    extra: String,
+    onExtraClick: () -> Unit,
+    total: String,
+    onCheckoutClick: () -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -44,95 +57,110 @@ fun CheckoutTicketComposable(
             Text(text = "Notes", style = Typography.labelMedium)
             Spacer(modifier = Modifier.weight(1f))
             /*TODO implement if in case user type a note*/
-            TextButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Filled.Add, contentDescription = null)
-                Text(text = "Add note")
+            if (note.isEmpty()) {
+
+                TextButton(onClick = { onAddNoteClick() }) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    Text(text = "Add note")
+                }
+            } else {
+                TextButton(onClick = { onAddNoteClick() }) {
+                    Icon(imageVector = Icons.Filled.Edit, contentDescription = null)
+                    Text(text = note.substring(0..10))
+                }
             }
         }
 
+    }
+
 //        OutlinedTextField(
-//            value = "",
+//            value = note,
 //            onValueChange = {},
 //            singleLine = false,
 //            modifier = Modifier
 //                .height(100.dp)
 //                .fillMaxWidth()
 //        )
-        //Products
-        Text(text = "Items", style = Typography.labelMedium)
-        ElevatedCard(
+    //Products
+    Text(text = "Items", style = Typography.labelMedium)
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+            .padding(5.dp),
+        elevation = CardDefaults.cardElevation(0.5.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f)
-                .padding(5.dp),
-            elevation = CardDefaults.cardElevation(0.5.dp),
-            colors = CardDefaults.cardColors(Color.White)
+                .padding(5.dp)
         ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(5.dp)
-            ) {
-                items(list) { product ->
-                    Row {
-                        Text(text = "${product.product.name}", modifier = Modifier.weight(3f))
-                        Text(text = "x${product.qty}", modifier = Modifier.weight(1f))
-                        Text(
-                            text = "$${product.subtotal}",
-                            modifier = Modifier.weight(1f),
-                            textAlign = TextAlign.End
-                        )
-                    }
+            items(list) { product ->
+                Row {
+                    Text(text = "${product.product.name}", modifier = Modifier.weight(3f))
+                    Text(text = "x${product.qty}", modifier = Modifier.weight(1f))
+                    Text(
+                        text = "$${product.subtotal}",
+                        modifier = Modifier.weight(1f),
+                        textAlign = TextAlign.End
+                    )
                 }
             }
         }
-        ElevatedCard(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
-            elevation = CardDefaults.cardElevation(0.5.dp),
-            colors = CardDefaults.cardColors(Color.White)
-        ) {
-            //Envio cost
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "Shipping")
-                Spacer(modifier = Modifier.weight(1f))
-                TextButton(onClick = { /*TODO*/ }) {
-                    Text(text = "11")
-                }
-            }
-
-            //Tax
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "Tax 16%")
-                Spacer(modifier = Modifier.weight(1f))
-                TextButton(onClick = { /*TODO*/ }) {
-                    Text(text = "11")
-                }
-            }
-            //Extra
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "Extra")
-                Spacer(modifier = Modifier.weight(1f))
-                TextButton(onClick = { /*TODO*/ }) {
-                    Text(text = "11")
-                }
-            }
-            //Total
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "Total")
-                Spacer(modifier = Modifier.weight(1f))
-                TextButton(onClick = { /*TODO*/ }) {
-                    Text(text = "11")
-                }
-            }
-        }
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color.Black)) {
-            Text(text = "Checkout", color = Color.White)
-        }
-        //State , save as
     }
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
+        elevation = CardDefaults.cardElevation(0.5.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        //Envio cost
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Shipping")
+            Spacer(modifier = Modifier.weight(1f))
+            TextButton(onClick = { onChangeShippingCostClick() }) {
+                Text(text = "$$shipping")
+            }
+        }
+
+        //Tax
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Tax 16%")
+            Spacer(modifier = Modifier.weight(1f))
+            TextButton(onClick = { onChangeTaxClick() }) {
+                Text(text = "$${tax}")
+            }
+        }
+        //Extra
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Extra")
+            Spacer(modifier = Modifier.weight(1f))
+            TextButton(onClick = { onExtraClick() }) {
+                Text(text = "$$extra")
+            }
+        }
+        //Total
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Total")
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "$${total}")
+
+        }
+    }
+    Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color.Black)) {
+        Text(text = "Checkout", color = Color.White)
+    }
+    //State , save as
 }
+}
+
+@Composable
+fun CheckoutTicketComposableImpl() {
+    CheckoutTicketComposable()
+}
+
 
 @Preview(showBackground = true)
 @Composable
