095f44cb09d0f14a399e9772091fb832ad459714
Abdiel Carreño <zayelmech@gmail.com>
2025-04-16 03:30:03 -0600

    ClientsListComposable Migrated to Material 3 ListItem and wrapped within Scaffold

--- demosales-ui/clients/src/main/java/com/imecatro/demosales/ui/clients/list/views/ClientsListComposable.kt
+++ demosales-ui/clients/src/main/java/com/imecatro/demosales/ui/clients/list/views/ClientsListComposable.kt
@@ -2,24 +2,19 @@ package com.imecatro.demosales.ui.clients.list.views
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -29,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
@@ -57,49 +51,44 @@ internal fun ClientCardCompose(
     onCardClicked: () -> Unit = {}
 ) {
     val view = LocalView.current
-    ElevatedCard(
+    Box(
         modifier = Modifier
-            .padding(2.dp, 2.dp)
             .clickable { onCardClicked() },
-        elevation = CardDefaults.cardElevation(0.5.dp)
     ) {
-        Row(
+        ListItem(
             modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally)
-                .padding(10.dp)
+                .fillMaxWidth(),
+            headlineContent = {
+                Text(text = client.name ?: "")
+            },
+            leadingContent = {
+                Image(
+                    painter = if (view.isInEditMode)
+                        painterResource(id = R.drawable.baseline_mood_24)
+                    else rememberAsyncImagePainter(
+                        ImageRequest.Builder(LocalContext.current)
+                            .data(client.imageUrl)
+                            .error(R.drawable.baseline_mood_24)
+                            .crossfade(true)
+                            .build()
 
-        ) {
-            // TODO add description and implement image by url
-
-            Image(
-                painter = if (view.isInEditMode)
-                    painterResource(id = R.drawable.baseline_mood_24)
-                else rememberAsyncImagePainter(
-                    ImageRequest.Builder(LocalContext.current)
-                        .data(client.imageUrl)
-                        .error(R.drawable.baseline_mood_24)
-                        .crossfade(true)
-                        .build()
-
-                ),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(80.dp)
-                    .align(Alignment.CenterVertically)
-                    .padding(5.dp)
-                    .clip(RoundedCornerShape(25)),
-                contentScale = ContentScale.FillWidth
-            )
-
-            Column {
-                Text(text = client.name ?: "", style = MaterialTheme.typography.titleMedium)
-                Spacer(modifier = Modifier.height(10.dp))
-                Text(text = client.number ?: "", style = MaterialTheme.typography.titleMedium)
-                Spacer(modifier = Modifier.height(10.dp))
-                Text(text = client.address ?: "", style = MaterialTheme.typography.titleMedium)
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(5.dp)
+                        .clip(RoundedCornerShape(25)),
+                    contentScale = ContentScale.FillWidth
+                )
+            },
+            supportingContent = {
+                Text(text = client.address ?: "")
+            },
+            overlineContent = {
+                Text(text = client.number ?: "")
             }
-        }
+
+        )
     }
 }
 
@@ -116,37 +105,29 @@ private fun ListOfClients(
         TopAppBar(title = {
             Text(text = "Clients")
         }, actions = {
-            IconButton(onClick = { onNavigateAction() }) {
-                Icon(imageVector = Icons.Filled.Add, contentDescription = null)
-            }
+
         })
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-//        contentPadding = innerPadding
-        ) {
-            if (isLoading) {
-//
-//            items(10) {
-//                ShimmerListItem(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .padding(10.dp)
-//                )
-//            }
+        Scaffold(floatingActionButton = {
+            FloatingActionButton(onNavigateAction) {
+                Icon(painter = painterResource(R.drawable.ic_add_client), contentDescription = null)
 
-            } else {
-
-
+            }
+        }) { paddingValues ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                contentPadding = paddingValues
+            ) {
                 items(list) { client ->
 
                     ClientCardCompose(client = client) { onCardClicked(client.id) }
                 }
             }
         }
+
     }
 
 }
