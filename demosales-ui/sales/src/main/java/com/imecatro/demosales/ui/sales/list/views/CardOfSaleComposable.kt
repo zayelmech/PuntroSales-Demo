f9488b203e75cf9f0043a88575788b8043ae295f
Abdiel Carreño <zayelmech@gmail.com>
2025-04-16 03:52:40 -0600

    Refactored SalesListComposable, colors hardcoded removed

--- demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/list/views/CardOfSaleComposable.kt
+++ demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/list/views/CardOfSaleComposable.kt
@@ -1,22 +1,15 @@
 package com.imecatro.demosales.ui.sales.list.views
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.imecatro.demosales.ui.sales.list.model.SaleOnListUiModel
-import com.imecatro.demosales.ui.theme.Typography
 
 @Preview(showBackground = true)
 @Composable
@@ -24,31 +17,24 @@ fun CardOfSaleComposable(
     sale: SaleOnListUiModel = SaleOnListUiModel(0, "Ab", date = "01/02/2022", 200.0, "f"),
     onCardClicked: () -> Unit = {}
 ) {
-
-    //val cardTag = "${ListProductsTestTags.CARD.tag}-${product.id}"
-
-    ElevatedCard(
+    Box(
         modifier = Modifier
-            .padding(2.dp, 2.dp)
-            .fillMaxWidth(0.9f)
-//            .width(350.dp)
             .clickable { onCardClicked() },
-        //.testTag(cardTag),
-        elevation = CardDefaults.cardElevation(0.5.dp),
-        colors = CardDefaults.cardColors(Color.White)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = sale.clientName ?: "", style = Typography.titleMedium)
-                Text(text = sale.date, fontSize = 18.sp)
+        ListItem(
+            modifier = Modifier.sizeIn(maxWidth = 411.dp),
+            headlineContent = {
+                Text(text = "$${sale.total}")
+            },
+            overlineContent = {
+                Text(text = sale.clientName)
+            },
+            supportingContent = {
+                Text(text = sale.date)
+            },
+            trailingContent = {
                 Text(text = sale.status)
             }
-            Text(text = "$${sale.total ?: 0.00}", style = Typography.titleMedium)
-        }
+        )
     }
 }
--- demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/list/views/SalesListComposable.kt
+++ demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/list/views/SalesListComposable.kt
@@ -4,24 +4,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.imecatro.demosales.ui.sales.list.model.SaleOnListUiModel
 import com.imecatro.demosales.ui.sales.list.viewmodel.SalesListViewModel
-import com.imecatro.demosales.ui.theme.BlueTurquoise80
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun SalesListComposable(
@@ -32,10 +27,8 @@ fun SalesListComposable(
     Scaffold(floatingActionButton = {
         FloatingActionButton(
             onClick = { onAddNewSale() },
-            containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = Color.White
         ) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = null)
         }
     }) { innerPadding ->
 
