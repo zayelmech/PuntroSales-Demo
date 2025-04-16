83d33d86b1f865ce67cf63627dfe93fe0f65f5aa
Abdiel Carreño <zayelmech@gmail.com>
2025-04-16 02:56:27 -0600

    fix: ProductsDao filter was getting all products on empty value

--- demosales-data/products/src/main/java/com/imecatro/products/data/datasource/ProductsDao.kt
+++ demosales-data/products/src/main/java/com/imecatro/products/data/datasource/ProductsDao.kt
@@ -23,6 +23,9 @@ interface ProductsDao {
     @Query("SELECT * FROM products_table WHERE id = :id ")
     fun getProductDetailsById(id: Int): ProductRoomEntity
 
-    @Query("SELECT * FROM products_table WHERE name LIKE :productName || '%'")
+    @Query("""
+    SELECT * FROM products_table 
+    WHERE (:productName != '' AND name LIKE :productName || '%')
+""")
     fun searchProducts(productName: String): Flow<List<ProductRoomEntity>>
 }
