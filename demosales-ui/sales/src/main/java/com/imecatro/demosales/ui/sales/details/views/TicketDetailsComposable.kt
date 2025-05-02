b602b23ef8c028a5f9e430048bbf86ba4f23c329
Abdiel Carreño <zayelmech@gmail.com>
2025-05-02 13:04:52 -0600

    TicketDetailsComposable refactored

--- demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/details/views/TicketDetailsComposable.kt
+++ demosales-ui/sales/src/main/java/com/imecatro/demosales/ui/sales/details/views/TicketDetailsComposable.kt
@@ -1,32 +1,50 @@
 package com.imecatro.demosales.ui.sales.details.views
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.imecatro.demosales.ui.sales.R
+import com.imecatro.demosales.ui.sales.details.model.ProductOnTicketUiModel
 import com.imecatro.demosales.ui.sales.details.model.TicketDetailsUiModel
 import com.imecatro.demosales.ui.sales.details.viewmodel.TicketDetailsViewModel
-import com.imecatro.demosales.ui.theme.ButtonFancy
 import com.imecatro.demosales.ui.theme.Typography
 import com.imecatro.demosales.ui.theme.dialogs.OnDeleteItemDialog
 
 @Preview(showBackground = true, showSystemUi = false)
 @Composable
 fun TicketDetailsComposable(
-    ticketDetails: TicketDetailsUiModel = TicketDetailsUiModel(listOf()),
+    ticketDetails: TicketDetailsUiModel = TicketDetailsUiModel(listOf(ProductOnTicketUiModel("a",1f,3f))),
     onDeleteClick: () -> Unit = {},
     onEditClick: () -> Unit = {}
 ) {
@@ -34,7 +52,7 @@ fun TicketDetailsComposable(
         modifier = Modifier
             .fillMaxSize()
             .padding(20.dp)
-            //.background(MaterialTheme.colorScheme.background)
+        //.background(MaterialTheme.colorScheme.background)
     ) {
 
         //Client
@@ -45,25 +63,23 @@ fun TicketDetailsComposable(
         }
         //Notes
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "Notes:", style = Typography.labelMedium)
+            Text(text = "Notes: ", style = MaterialTheme.typography.labelMedium)
             Spacer(modifier = Modifier.weight(1f))
             Text(text = ticketDetails.note)
         }
 
         //Products
-        Text(text = "Items:", style = Typography.labelMedium)
+        Text(text = "Items:", style = MaterialTheme.typography.titleSmall)
         ElevatedCard(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f)
-                .padding(5.dp),
-            elevation = CardDefaults.cardElevation(0.5.dp),
-            colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surface)
+                .weight(1f),
+            elevation = CardDefaults.cardElevation(0.5.dp)
         ) {
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(5.dp)
+                    .padding(10.dp)
             ) {
                 items(ticketDetails.list) { product ->
                     Row {
@@ -78,50 +94,46 @@ fun TicketDetailsComposable(
                 }
             }
         }
-        ElevatedCard(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
-            elevation = CardDefaults.cardElevation(0.5.dp),
-            colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surface)
-        ) {
-            //Shipping cost
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "Shipping")
-                Spacer(modifier = Modifier.weight(1f))
-                Text(text = "$${ticketDetails.shippingCost}")
-            }
-//            //Tax
-//            Row(verticalAlignment = Alignment.CenterVertically) {
-//                Text(text = "Tax 16%")
-//                Spacer(modifier = Modifier.weight(1f))
-//                Text(text = "$${ticketDetails.tax}")
-//            }
-            //Extra
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "Extra")
-                Spacer(modifier = Modifier.weight(1f))
-                Text(text = "$${ticketDetails.extra}")
-            }
-            //Total
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "Total")
-                Spacer(modifier = Modifier.weight(1f))
-                Text(text = "$${ticketDetails.total}")
-
-            }
+        HorizontalDivider()
+        Spacer(modifier = Modifier.size(5.dp))
+        //Shipping cost
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Shipping")
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "$${ticketDetails.shippingCost}")
         }
-
-        ButtonFancy(
-            text = "Edit", color = MaterialTheme.colorScheme.primary, icon = Icons.Filled.Edit
-        ) {
-            onEditClick()
+        //Extra
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Extra")
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "$${ticketDetails.extra}")
         }
+        //Total
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Total")
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "$${ticketDetails.total}")
+        }
+        Spacer(modifier = Modifier.size(20.dp))
 
-        ButtonFancy(
-            text = "Delete", color = MaterialTheme.colorScheme.secondary, icon = Icons.Filled.Delete
-        ) {
-            onDeleteClick()
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Button(
+                onClick = { onEditClick() },
+                modifier = Modifier
+                    .sizeIn(maxWidth = 320.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("New Sale")
+            }
+
+            TextButton(
+                onClick = onDeleteClick,
+                colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.error)
+            ) {
+                Icon(Icons.Default.Delete, null)
+                Text("Cancel")
+            }
         }
     }
 }
