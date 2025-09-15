68340abac983d310ec55c0a27ea981aee0c80725
Abdiel Carreño <zayelmech@gmail.com>
2025-09-15 01:49:10 -0600

    ft! adding filter options to product list New model `OrderedFilterUiModel` and string resources for filter labels were added. `ProductsViewModel` now handles filter state and applies sorting to the product list based on selected filter. `ListProductsComposables` updated to display filter chips and a modal bottom sheet for selecting order preferences. Added a new drawable resource for filter icon.

--- demosales-ui/products/src/main/java/com/imecatro/products/ui/list/viewmodels/ProductsViewModel.kt
+++ demosales-ui/products/src/main/java/com/imecatro/products/ui/list/viewmodels/ProductsViewModel.kt
@@ -5,21 +5,25 @@ import com.imecatro.demosales.domain.products.repository.ProductsRepository
 import com.imecatro.demosales.ui.theme.architect.BaseViewModel
 import com.imecatro.demosales.ui.theme.architect.ErrorUiModel
 import com.imecatro.products.ui.list.mappers.toProductUiModel
+import com.imecatro.products.ui.list.model.OrderedFilterUiModel
 import com.imecatro.products.ui.list.model.ProductUiModel
+import com.imecatro.products.ui.list.model.checkElement
 import com.imecatro.products.ui.list.uistate.ListProductsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -29,19 +33,40 @@ class ProductsViewModel @Inject constructor(
     private val iODispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : BaseViewModel<ListProductsUiState>(ListProductsUiState.idle) {
 
+    private val _filtersState: MutableStateFlow<List<OrderedFilterUiModel>> =
+        MutableStateFlow(OrderedFilterUiModel.filters)
+
+    val filtersState: StateFlow<List<OrderedFilterUiModel>> = _filtersState.asStateFlow()
+
     val productsList: StateFlow<List<ProductUiModel>> =
         productsRepository.getAllProducts()
             .onStart {
                 updateState { copy(isFetchingProducts = true) }
-            }.catch {
+            }.onEach {
+                updateState { copy(isFetchingProducts = false) }
+            }.map { it.toProductUiModel() }
+            .combine(filtersState) { products, filters ->
+                // Filter by order
+
+                val orderSelected: OrderedFilterUiModel = filters.first { it.isChecked }
+
+                when (orderSelected.type) {
+                    OrderedFilterUiModel.Type.NAME -> products.sortedBy { filter -> filter.name }
+                    OrderedFilterUiModel.Type.PRICE -> products.sortedBy { filter -> filter.price }
+                    OrderedFilterUiModel.Type.STOCK -> products.sortedBy { filter -> filter.stock }
+                    OrderedFilterUiModel.Type.DATE -> products.sortedBy { filter -> filter.id }
+                    OrderedFilterUiModel.Type.NAME_INVERSE -> products.sortedByDescending { filter -> filter.name }
+                    OrderedFilterUiModel.Type.PRICE_INVERSE -> products.sortedByDescending { filter -> filter.price }
+                    OrderedFilterUiModel.Type.STOCK_INVERSE -> products.sortedByDescending { filter -> filter.stock }
+                    OrderedFilterUiModel.Type.DATE_INVERSE -> products.sortedByDescending { filter -> filter.id }
+                }
+            }
+            .catch {
                 updateState { copy(isFetchingProducts = false) }
 
                 val error = ErrorUiModel("", "No data")
                 updateState { copy(errorFetchingProducts = error) }
-            }.onEach {
-                updateState { copy(isFetchingProducts = false) }
-            }.map { it.toProductUiModel() }
-            .flowOn(iODispatcher)
+            }
             .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
 
@@ -52,6 +77,12 @@ class ProductsViewModel @Inject constructor(
             updateState { copy(productsFiltered = list.toProductUiModel()) }
         }
 
+    }
+
+    fun onFilterChange(filter: OrderedFilterUiModel) {
+        viewModelScope.launch(iODispatcher) {
+            _filtersState.update { it.checkElement(filter) }
+        }
     }
 
 }
--- demosales-ui/products/src/main/java/com/imecatro/products/ui/list/views/ListProductsComposables.kt
+++ demosales-ui/products/src/main/java/com/imecatro/products/ui/list/views/ListProductsComposables.kt
@@ -1,11 +1,15 @@
 package com.imecatro.products.ui.list.views
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,28 +18,37 @@ import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -44,9 +57,12 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -56,6 +72,7 @@ import com.imecatro.demosales.ui.theme.architect.UiStateHandler
 import com.imecatro.demosales.ui.theme.architect.isLoading
 import com.imecatro.demosales.ui.theme.common.formatAsCurrency
 import com.imecatro.products.ui.R
+import com.imecatro.products.ui.list.model.OrderedFilterUiModel
 import com.imecatro.products.ui.list.model.ProductUiModel
 import com.imecatro.products.ui.list.viewmodels.ProductsViewModel
 import kotlinx.coroutines.FlowPreview
@@ -73,6 +90,8 @@ fun ListOfProducts(
     isLoading: Boolean = false,
     onSearchProduct: (String) -> Unit = {},
     searchList: List<ProductUiModel> = emptyList(),
+    orderList: List<OrderedFilterUiModel> = OrderedFilterUiModel.filters,
+    onCheckedChange: (OrderedFilterUiModel) -> Unit = {},
     onCardClicked: (Long?) -> Unit = {},
     onNavigateAction: () -> Unit = {},
 ) {
@@ -80,6 +99,16 @@ fun ListOfProducts(
     var expanded by rememberSaveable { mutableStateOf(false) }
 
     val scrollState = rememberLazyListState()
+
+    var showFilters by remember { mutableStateOf(false) }
+
+    var orderedFilter by remember { mutableStateOf(false) }
+
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+        // Add this line to fix the error:
+        confirmValueChange = { true }, // Add a default confirmValueChange lambda
+    )
 
     LaunchedEffect(text) {
         snapshotFlow { text }
@@ -91,11 +120,12 @@ fun ListOfProducts(
 
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = { onNavigateAction() },
-            ) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = null)
-            }
+            if (!expanded)
+                FloatingActionButton(
+                    onClick = { onNavigateAction() },
+                ) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
         },
         topBar = {
             Row(
@@ -104,29 +134,41 @@ fun ListOfProducts(
             ) {
                 SearchBar(
                     inputField = {
-                    SearchBarDefaults.InputField(
-                        query = text,
-                        onQueryChange = { text = it },
-                        onSearch = { onSearchProduct(text) },
-                        expanded = expanded,
-                        onExpandedChange = { expanded = it },
-                        placeholder = { Text("Search a product") },
-                        leadingIcon = {
-                            if (expanded) {
-                                IconButton(onClick = {
-                                    expanded = false
-                                }) {
-                                    Icon(
-                                        Icons.AutoMirrored.Filled.ArrowBack,
-                                        contentDescription = null
-                                    )
+                        SearchBarDefaults.InputField(
+                            query = text,
+                            onQueryChange = { text = it },
+                            onSearch = { onSearchProduct(text) },
+                            expanded = expanded,
+                            onExpandedChange = { expanded = it },
+                            placeholder = { Text("Search a product") },
+                            leadingIcon = {
+                                if (expanded) {
+                                    IconButton(onClick = {
+                                        expanded = false
+                                    }) {
+                                        Icon(
+                                            Icons.AutoMirrored.Filled.ArrowBack,
+                                            contentDescription = null
+                                        )
+                                    }
+                                } else {
+                                    Icon(Icons.Default.Search, contentDescription = null)
                                 }
-                            } else {
-                                Icon(Icons.Default.Search, contentDescription = null)
+                            },
+                            trailingIcon = {
+                                if (!expanded)
+                                    IconButton(
+                                        onClick = {
+                                            showFilters = !showFilters
+                                        },
+                                        colors = IconButtonDefaults.iconButtonColors(containerColor = if (showFilters) MaterialTheme.colorScheme.primaryContainer else Color.Unspecified)
+                                    ) {
+                                        Icon(painterResource(R.drawable.sliders), null)
+                                    }
                             }
-                        }
-                    )
-                }, expanded = expanded, onExpandedChange = { expanded = it },
+
+                        )
+                    }, expanded = expanded, onExpandedChange = { expanded = it },
                     content = {
                         LazyColumn(
                             modifier = Modifier.sizeIn(maxWidth = 411.dp),
@@ -141,39 +183,80 @@ fun ListOfProducts(
             }
 
         }) { innerPadding ->
-
-        LazyColumn(
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            state = scrollState,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            contentPadding = innerPadding
+                .fillMaxWidth()
+                .padding(innerPadding),
         ) {
-            if (isLoading) {
+            AnimatedVisibility(visible = showFilters) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(rememberScrollState()),
+                    horizontalArrangement = Arrangement.Start,
+                ) {
+                    Spacer(modifier = Modifier.size(15.dp))
 
-                items(10) {
-                    ShimmerListItem(
-                        modifier = Modifier
-                            .fillMaxWidth()
+                    FilterChip(
+                        onClick = { orderedFilter = true },
+                        label = {
+                            Text("Order by")
+                        },
+                        selected = orderList.any { it.isChecked },
+                        leadingIcon = {
+                            if (orderList.any { it.isChecked }) {
+                                Icon(Icons.Filled.Done, null)
+                            } else {
+                                Icon(Icons.Filled.ArrowDropDown, null)
+                            }
+                        }
                     )
-                }
 
-            } else {
-                items(list) { product ->
-
-                    ProductCardCompose(product = product) { onCardClicked(product.id) }
-                    HorizontalDivider()
                 }
-//                if (list.isEmpty()){
-//                    item {
-//                        Text(text = "There is no products yet, to start to run your business you need to add a product, click the button (+) below to do it")
-//
-//                        // TODO if you prefer, import from excel file or connect with your backend to get the products
-//                    }
-//                }
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                state = scrollState,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                if (isLoading) {
+
+                    items(10) {
+                        ShimmerListItem(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                    }
+
+                } else {
+                    items(list) { product ->
+
+                        ProductCardCompose(product = product) { onCardClicked(product.id) }
+                        HorizontalDivider()
+                    }
+                }
             }
         }
     }
+
+    if (orderedFilter)
+        ModalBottomSheet(onDismissRequest = { orderedFilter = false }, sheetState = sheetState) {
+            LazyColumn {
+                items(orderList) { ordered ->
+                    ListItem(
+                        leadingContent = {
+                            RadioButton(selected = ordered.isChecked, onClick = {
+                                onCheckedChange(ordered)
+                            })
+                        },
+                        headlineContent = {
+                            Text(text = stringResource(ordered.text))
+                        }
+                    )
+                }
+            }
+        }
 }
 
 @Composable
@@ -244,11 +327,16 @@ fun ListOfProductsStateImpl(
     val productsList by productsViewModel.productsList.collectAsState()
     val uiState by productsViewModel.uiState.collectAsState()
 
+    val filters by productsViewModel.filtersState.collectAsState()
+
+
     ListOfProducts(
         list = productsList.toMutableStateList(),
         isLoading = uiState.isLoading,
         searchList = uiState.productsFiltered,
         onSearchProduct = { productsViewModel.onSearchAction(it) },
+        orderList = filters,
+        onCheckedChange = { productsViewModel.onFilterChange(it) },
         onCardClicked = {
             scope.launch { onNavigateAction(it) }
         },
