f36748d879e224e59e27755e4b3db457575ee2f1
Abdiel Carreño <zayelmech@gmail.com>
2026-05-08 01:04:52 -0600

    build: Bump version code to 23 and version name to 1.14.2
    - Increments `versionCode` from 22 to 23.
    - Updates `versionName` from 1.14.1 to 1.14.2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.imecatro.demosales"
         minSdk = 26
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 22
-        versionName = "1.14.1"
+        versionCode = 23
+        versionName = "1.14.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
